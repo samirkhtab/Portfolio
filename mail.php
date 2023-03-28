4cff7a3da889b07789160dee421c1331c290cec1
Samir <50784400+samirkhtab@users.noreply.github.com>
2023-03-28 23:04:26 +0200

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php 
+
+if (isset($_POST['submit'])) {
+    $name =$_POST['name'];
+    $subject =$_POST['subject'];
+    $mailForm =$_POST['mail'];
+    $message =$_POST['message'];
+    
+
+    $mailTo = "samerkhtab67@gmail.com"
+    $headers = "From: ".$mailForm;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+
+    mail( $mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
+
+
